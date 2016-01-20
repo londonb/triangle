8abf5f16aaebe07f618ba372503b26210c1f62e0
Brad London and Jess Guilford <student@epicodus.com>
2016-01-19 16:40:16 -0800

    add html

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,26 @@ var scaleneTest = function(sidea, sideb, sidec) {
     return false;
   }
 };
+
+$(document).ready(function() {
+
+  $("form#triangle-quiz").submit(function(event) {
+    var sidea = $("input#sidea").val();
+    var sideb = parseInt($("input#sideb").val());
+    var sidec = parseInt($("input#sidec").val());
+    var equilateral = equilateralTest(sidea, sideb, sidec);
+  console.log(equilateralTest);
+    if (equilateral = true) {
+      $("#equilateral").text("an equilateral ");
+    } else {
+      alert("Arrgh");
+    }
+
+    $(".results").show();
+
+    event.preventDefault();
+  });
+});
 /*
 if (sidea === sideb === sidec){
   return true;
